17d98c910c13cc9a24d3dc91c4b079f7bc0eeaf9
chen.kai <m15252734552@163.com>
2025-05-15 17:53:31 +0800

    build: 更新项目路径和引用- 修改 defaultSetting.json 中的 output 路径 - 更新 download-template.js、generate-api.js 和 get-help.js 中对 defaultSetting.json 的引用路径

--- generate-api.js
+++ generate-api.js
@@ -4,13 +4,13 @@ const handleError = require("./handleError");
 const deleteFolder = require("./deleteFolder");
 const copyFile = require("./copyFile");
 
-const java = require('defaultSetting.json').java;
+const java = require('./defaultSetting.json').java;
 
 const jar = path.join(__dirname, './openapi-generator-cli-7.13.0.jar')
 
-const doc = require('defaultSetting.json').doc
+const doc = require('./defaultSetting.json').doc
 
-const output = require('defaultSetting.json').output
+const output = require('./defaultSetting.json').output
 
 exec(`${ java } -jar ${ jar } generate -i ${ doc } -g javascript -o ${ output } --skip-validate-spec --template-dir ./custom-templates --global-property=models,apis --additional-properties=usePromises=true,sourceFolder=/`,
     (error, stdout, stderr) => handleError(error, stdout, stderr, () => {
